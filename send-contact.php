acc13ddf04ab1fd9a889a9df5f431b27435ac425
Alen Simec <alen@eburza.hr>
2017-10-29 18:24:51 +0100

    Update v2.0

--- send-contact.php
+++ send-contact.php
@@ -57,13 +57,52 @@
 		<div id="contact">
 			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2781.7890741539636!2d15.966758816056517!3d45.795453279106205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4765d68b5d094979%3A0xda8bfa8459b67560!2sUl.+Vrbik+VIII%2C+10000%2C+Zagreb!5e0!3m2!1shr!2shr!4v1509296660756" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
 			<?php
-				print '<p style="text-align:center; padding: 5px; background-color: #d7d6d6;">We recieved your question. We will answer within 24 hours.</p>
-				<p>First name: ' . $_POST['firstname'] . '</p>
+				print '<p style="text-align:center; padding: 10px; background-color: #d7d6d6;border-radius: 5px;">We recieved your question. We will answer within 24 hours.</p>';
+				$EmailHeaders  = "MIME-Version: 1.0\r\n";
+				$EmailHeaders .= "Content-type: text/html; charset=utf-8\r\n";
+				$EmailHeaders .= "From: <alen@tvz.hr>\r\n";
+				$EmailHeaders .= "Reply-To:<alen@eburza.hr>\r\n";
+				$EmailHeaders .= "X-Mailer: PHP/".phpversion();
+				$EmailSubject = 'Example page - Contact Form';
+				$EmailBody  = '
+				<html>
+				<head>
+				   <title>'.$EmailSubject.'</title>
+				   <style>
+					body {
+					  background-color: #ffffff;
+						font-family: Arial, Helvetica, sans-serif;
+						font-size: 16px;
+						padding: 0px;
+						margin: 0px auto;
+						width: 500px;
+						color: #000000;
+					}
+					p {
+						font-size: 14px;
+					}
+					a {
+						color: #00bad6;
+						text-decoration: underline;
+						font-size: 14px;
+					}
+					
+				   </style>
+				   </head>
+				<body>
+					<p>First name: ' . $_POST['firstname'] . '</p>
+					<p>Last name: ' . $_POST['lastname'] . '</p>
+					<p>E-mail: <a href="mailto:' . $_POST['email'] . '">' . $_POST['email'] . '</a></p>
+					<p>Country: ' . $_POST['country'] . '</p>
+					<p>Subject: ' . $_POST['subject'] . '</p>
+				</body>
+				</html>';
+				print '<p>First name: ' . $_POST['firstname'] . '</p>
 				<p>Last name: ' . $_POST['lastname'] . '</p>
 				<p>E-mail: ' . $_POST['email'] . '</p>
 				<p>Country: ' . $_POST['country'] . '</p>
 				<p>Subject: ' . $_POST['subject'] . '</p>';
-				mail($_POST['email'],'Example page - message',$_POST['subject']);
+				mail($_POST['email'], $EmailSubject, $EmailBody, $EmailHeaders);
 			?>
 		</div>
 	</main>
