c0708d1bd1e1f3ab8e147b0651a8bdba46310ce6
Alen Šimec <alen@eburza.hr>
2018-01-31 09:25:49 +0100

    Update send-contact.php

--- send-contact.php
+++ send-contact.php
@@ -109,17 +109,6 @@
 	<footer>
 		<p>Copyright &copy; 2017 Alen Šimec</p>
 	</footer>
-	<!-- Google Analytics code -->
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-98455037-1', 'auto');
-	  ga('send', 'pageview');
-
-	</script>
-	<!-- End Google Analytics code -->
+	
 </body>
 </html>
